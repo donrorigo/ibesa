d27778784f0e3d51cf09538cdee62f4515077fea
Rodrigo Puerto <rpuertopedrera@gmail.com>
2019-08-26 22:01:55 +0200

    End

--- code/main.sh
+++ code/main.sh
@@ -7,12 +7,12 @@ while read field1 field2 field3; do
   ./IBESA\ convert\ to\ HYPERVOLUME $field1.txt
   ./normalize RESULTADOS\:\ $field1 $field1
   ./hyp_ind hyp_ind_param.txt NORMALIZADO\:\ $field1 null.txt output.txt
-  echo "$field1:$(./convert2decimal output.txt)" >> "hypervolume_rep$1.txt"
+  echo "$field1: $(./convert2decimal output.txt)" >> "hypervolume_rep$1.txt"
   rm $field1.txt RESULTADOS\:\ $field1 NORMALIZADO\:\ $field1 output.txt
 done < proteins.txt
 }
 
 for i in {1..5}
 do
-rep i
+rep $i
 done
