b3cb76263e43ea19d868da1c0cc9a89375687a76
Rodrigo Puerto Pedrera <rpuertopedrera@gmail.com>
2019-08-27 21:28:41 +0200

    updated main

--- code/main.sh
+++ code/main.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-function rep()
-{
-while read field1 field2 field3; do
-  ./ibesa 100 100 10 $field1 $field2 $field3
-  ./IBESA\ convert\ to\ HYPERVOLUME $field1.txt
-  ./normalize RESULTADOS\:\ $field1 $field1
-  ./hyp_ind hyp_ind_param.txt NORMALIZADO\:\ $field1 null.txt output.txt
-  echo "$field1: $(./convert2decimal output.txt)" >> "hypervolume_rep$1.txt"
-  rm RESULTADOS\:\ $field1 NORMALIZADO\:\ $field1 output.txt
-done <= proteins.txt
-}
-
 mkdir "IBEA Results"
-cd "IBEA Results"
 for i in {1..5}
 do
-mkdir Rep$i
-cd Rep$i
-rep $i
-cd ..
+  mkdir IBEA\ Results/Rep$i
+  while read field1 field2 field3; do
+    ./ibesa 100 100 10 $field1 $field2 $field3
+    ./IBESA\ convert\ to\ HYPERVOLUME $field1.txt
+    ./normalize RESULTADOS\:\ $field1 $field1
+    ./hyp_ind hyp_ind_param.txt NORMALIZADO\:\ $field1 null.txt output.txt
+    echo "$field1: $(./convert2decimal output.txt)" >> "hypervolumes.txt"
+    rm RESULTADOS\:\ $field1 output.txt
+    mv $field1.txt NORMALIZADO\:\ $field1 IBEA\ Results/Rep$i
+  done < proteins.txt
+  mv "hypervolumes.txt" IBEA\ Results/Rep$i
 done
